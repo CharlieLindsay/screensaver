ec3818f145be42d231cd2ffaaef1d56ef50899ff
Charles Lindsay <186342@cloud.rangitoto.school.nz>
2026-07-05 18:49:39 +1200

    starting writing calculations for moon position - using all my brain power :(

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
-const video_moon = document.getElementById("moonvideo");
+const viewportWidth = window.innerWidth; 
 function timeandvideo() {
     now = new Date();
     hours = now.getHours();
-    console.log(hours);
-    if (hours > 19 || hours < 6){
+    //console.log(hours);
+    minutes = now.getMinutes();
+    //console.log(minutes);
+    minutes_into_day = (hours * 60 + minutes)
+    console.log(minutes_into_day)
+    if (minutes_into_day > 1100 || minutes_into_day < 1100){
         document.getElementById("moon").style.display = "block";
         document.getElementById("sun").style.display = "none";
+        minutes_in_night = (1440 - 1100 + 1100);
+        if (minutes_into_day > 1100){
+            partition = (minutes_into_day - 1100)
+        }
+        else{
+            partition = (340 + )
+        }
+        console.log(position);
+        document.getElementById("moon").style.left = "";
         console.log("Night");
     } 
     else{
