53ffef9efc79c3fc66cd8d055734a0b2a99c5252
Charles Lindsay <186342@cloud.rangitoto.school.nz>
2026-07-05 19:00:34 +1200

    got main calculations done, but position isn't working

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const viewportWidth = window.innerWidth; 
+const viewportWidth = window.innerWidth;
 function timeandvideo() {
     now = new Date();
     hours = now.getHours();
@@ -7,25 +7,31 @@ function timeandvideo() {
     //console.log(minutes);
     minutes_into_day = (hours * 60 + minutes)
     console.log(minutes_into_day)
-    if (minutes_into_day > 1100 || minutes_into_day < 1100){
+    if (minutes_into_day > 1140 || minutes_into_day < 360) {
         document.getElementById("moon").style.display = "block";
         document.getElementById("sun").style.display = "none";
-        minutes_in_night = (1440 - 1100 + 1100);
-        if (minutes_into_day > 1100){
-            partition = (minutes_into_day - 1100)
+        if (minutes_into_day > 1140) {
+            partition = (minutes_into_day - 1140)
         }
-        else{
-            partition = (340 + )
+        else {
+            partition = (340 + 360 - minutes_into_day)
         }
-        console.log(position);
-        document.getElementById("moon").style.left = "";
+        console.log(partition);
+        points_to_change_width = viewportWidth / 720
+        width = (points_to_change_width * partition)
+        document.getElementById("moon").style.left = width;
         console.log("Night");
-    } 
-    else{
+    }
+    else {
         document.getElementById("moon").style.display = "none";
         document.getElementById("sun").style.display = "block";
         console.log("Day");
+        partition = (minutes_into_day - 360)
+        console.log(partition);
+        points_to_change_width = viewportWidth / 780
+        width = (points_to_change_width * partition)
+        document.getElementById("sun").style.left = width;
     }
-    
+
 }
 const intervalId = setInterval(timeandvideo, 1000);
