91cb260d1f28b8bd40256f6b8d5c917a2c2eb403
Charles Lindsay <186342@cloud.rangitoto.school.nz>
2026-07-05 19:11:14 +1200

    Horizontal position of images

--- script.js
+++ script.js
@@ -1,37 +1,38 @@
-const viewportWidth = window.innerWidth;
 function timeandvideo() {
+    viewportWidth = window.innerWidth;
+    imageWidth = 300;
+    travelDistance = viewportWidth - imageWidth;
     now = new Date();
     hours = now.getHours();
-    //console.log(hours);
     minutes = now.getMinutes();
-    //console.log(minutes);
-    minutes_into_day = (hours * 60 + minutes)
-    console.log(minutes_into_day)
+    minutes_into_day = (hours * 60 + minutes);
+
     if (minutes_into_day > 1140 || minutes_into_day < 360) {
         document.getElementById("moon").style.display = "block";
         document.getElementById("sun").style.display = "none";
+
         if (minutes_into_day > 1140) {
-            partition = (minutes_into_day - 1140)
+            partition = (minutes_into_day - 1140);
+        } else {
+            partition = (minutes_into_day + (1440 - 360));
         }
-        else {
-            partition = (340 + 360 - minutes_into_day)
-        }
-        console.log(partition);
-        points_to_change_width = viewportWidth / 720
-        width = (points_to_change_width * partition)
-        document.getElementById("moon").style.left = width;
-        console.log("Night");
+        points_to_change_width = travelDistance / 660;
+        width = (points_to_change_width * partition);
+        document.getElementById("moon").style.left = width + "px";
+        console.log("Night", partition, width);
     }
     else {
         document.getElementById("moon").style.display = "none";
         document.getElementById("sun").style.display = "block";
-        console.log("Day");
-        partition = (minutes_into_day - 360)
-        console.log(partition);
-        points_to_change_width = viewportWidth / 780
-        width = (points_to_change_width * partition)
-        document.getElementById("sun").style.left = width;
-    }
 
+        partition = (minutes_into_day - 360);
+
+        points_to_change_width = travelDistance / 780;
+        width = (points_to_change_width * partition);
+        document.getElementById("sun").style.left = width + "px";
+        console.log("Day", partition, width);
+    }
 }
+
 const intervalId = setInterval(timeandvideo, 1000);
+timeandvideo();
